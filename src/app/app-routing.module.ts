033df2a6f85ad7dc460897c8ae6c8f3b84034a7e
vipinms <mjvipinms@gmail.com>
2022-06-22 11:41:18 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdvanceSearchComponent } from './advance-search/advance-search.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'login' , component :LoginComponent},
+  {path :'search' , component :AdvanceSearchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
